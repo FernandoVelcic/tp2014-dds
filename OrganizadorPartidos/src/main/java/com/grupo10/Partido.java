264fe0901899f4c31ace2784a03defd038e0f544
FernandoVelcic <fernando_velcic_77@hotmail.com>
2014-05-01 16:17:37 -0300

    Pequeños cambios a Partido, UML agregado y pom modificado.

--- OrganizadorPartidos/src/main/java/com/grupo10/Partido.java
+++ OrganizadorPartidos/src/main/java/com/grupo10/Partido.java
@@ -1,29 +1,34 @@
 package com.grupo10;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
 import com.grupo10.Modalidad.*;
 
 public class Partido {
-	List<Participante> participantes = new ArrayList<Participante>();	
+	List<Participante> participantes = new ArrayList<Participante>();
+	Date diaYhora;
 	
-	public boolean inscribirJugador(Jugador jugador, Modalidad modalidad){
-		if(calcularConfirmados() > 10)
+	public Partido(Date diaYhora) {
+		this.diaYhora = diaYhora;
+	}
+
+	public boolean inscribirJugador(Jugador jugador, Modalidad modalidad) {
+		if (calcularConfirmados() > 10)
 			return false;
-			
+
 		participantes.add(new Participante(jugador, modalidad));
 		return true;
 	}
-	
-	public Stream<Participante> obtenerConfirmados(){
+
+	public Stream<Participante> obtenerConfirmados() {
 		return participantes.stream().filter(p -> p.isConfirmado());
 	}
-	
-	public int calcularConfirmados(){
-		return (int)obtenerConfirmados().count();
+
+	public int calcularConfirmados() {
+		return (int) obtenerConfirmados().count();
 	}
-	
-	
+
 }
