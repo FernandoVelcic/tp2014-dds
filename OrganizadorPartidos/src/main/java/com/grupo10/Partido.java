b02cc78f6f6e5e224ed2c187dd95254974d8e340
FernandoVelcic <fernando_velcic_77@hotmail.com>
2014-05-02 16:23:13 -0300

    Varios cambios en Partido

--- OrganizadorPartidos/src/main/java/com/grupo10/Partido.java
+++ OrganizadorPartidos/src/main/java/com/grupo10/Partido.java
@@ -1,6 +1,8 @@
 package com.grupo10;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
@@ -8,10 +10,10 @@ import java.util.stream.Stream;
 public class Partido {
 	List<Participante> participantes = new ArrayList<Participante>();
 	Date diaYhora;
-	
+
 	Equipo equipo1;
 	Equipo equipo2;
-	
+
 	public Partido(Date diaYhora) {
 		this.diaYhora = diaYhora;
 	}
@@ -23,33 +25,49 @@ public class Partido {
 	public boolean inscribirJugadorCondicional(Jugador jugador) {
 		return inscribirJugador(jugador, 2);
 	}
-	
+
 	public boolean inscribirJugadorSolidario(Jugador jugador) {
 		return inscribirJugador(jugador, 3);
 	}
-	
-	public boolean inscribirJugador(Jugador jugador, int prioridadModalidad) {
-		if(calcularConfirmados() > 10)
-			return false;
-	
-		participantes.add(new Participante(jugador, prioridadModalidad));
-		
-		return true;
-	}
-	
-	public Stream<Participante> obtenerConfirmados() {
-		return participantes.stream().filter(p -> p.prioridadModalidad == 1);
-	}
-
-	public int calcularConfirmados() {
-		return (int) obtenerConfirmados().count();
-	}
 
 	public void GenerarEquipos() {
-		// TODO Auto-generated method stub
+		ordenarPorModalidad();
+		
+		//Obtener los primeros 10
+		//TODO Validar que sean los primeros 10 que pueden cumplir la condicion
+		List<Participante> jugadores = new ArrayList<Participante>(participantes.subList(0, 10));
+		// TODO Criterio para llenar equipo1 y equipo2
 	}
 
 	public void ConfirmarEquipos() {
 		// TODO Auto-generated method stub
+	}
+	
+	private boolean inscribirJugador(Jugador jugador, int prioridadModalidad) {
+		if (calcularConfirmados() > 10)
+			return false;
+
+		participantes.add(new Participante(jugador, prioridadModalidad));
+
+		return true;
+	}
+
+	private Stream<Participante> obtenerConfirmados() {
+		return participantes.stream().filter(p -> p.prioridadModalidad == 1);
+	}
+
+	private int calcularConfirmados() {
+		return (int) obtenerConfirmados().count();
+	}
+	
+	private void ordenarPorModalidad()
+	{
+		Collections.sort(participantes, new Comparator<Participante>() {
+	        @Override
+	        public int compare(Participante participante1, Participante participante2)
+	        {
+	            return participante1.prioridadModalidad - participante2.prioridadModalidad;
+	        }}
+		);
 	}
 }
