1e7e84416671d52168ee1d46fa262d63bbfeac50
Carla Madeo <carlamadeo@gmail.com>
2014-05-25 01:12:36 -0300

    Se agregan Test para las infracciones

---
+++ src/test/java/com/grupo10/test/TestInfracciones.java
@@ -0,0 +1,42 @@
+package com.grupo10.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.grupo10.Participante;
+import com.grupo10.Partido;
+import com.grupo10.Administrador;
+
+public class TestInfracciones {
+
+	Participante martin;
+	Participante tomas;
+	Partido partido;
+	
+	@Before
+	public void setUp() throws Exception {
+		martin = new Participante();
+		tomas = new Participante();
+		partido = new Partido(new Date(),new Administrador());
+	}
+
+	@Test
+	public void testMartinSeDaDeBajaDelPartidoSinProponerReemplazoYSeLeGeneraUnaInfraccion() {
+		partido.inscribirJugador(martin);
+		partido.generarJugadores();
+		partido.darBajaJugador(martin);
+		assertTrue(martin.infracciones.size() == 1);
+	}
+	
+	@Test
+	public void testMartinSeDaDeBajaDelPartidoYProponerReemplazoNoSeLeGeneraUnaInfraccion() {
+		partido.inscribirJugador(martin);
+		partido.generarJugadores();
+		partido.darBajaJugadorYProponerReemplazo(martin, tomas);
+		assertTrue(martin.infracciones.size() == 0);
+	}
+}
