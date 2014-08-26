d6b03259d517f011081d2dba9a48f7ca26258543
FernandoVelcic <fernando_velcic_77@hotmail.com>
2014-08-26 20:31:05 -0300

    Actualizando JugadorView

--- src/main/java/com/grupo10/ui/JugadorView.java
+++ src/main/java/com/grupo10/ui/JugadorView.java
@@ -11,27 +11,27 @@ import com.grupo10.juego.Participante;
 
 
 public class JugadorView extends Window<JugadorViewModel> {
-	private Participante jugador;
 	
 	public JugadorView(WindowOwner owner, Participante jugador) {
-		super(owner, new JugadorViewModel());
-		this.jugador = jugador;
+		super(owner, new JugadorViewModel(jugador));
 	}
 
 	@Override
 	public void createContents(Panel mainPanel) {
-		setTitle("Informacion del jugador: " + jugador.getNombre());
+		setTitle("Informacion del jugador: " + getModelObject().getNombre());
 		mainPanel.setLayout(new VerticalLayout());
 		
-		new Label(mainPanel).setText("Nombre");
-		new Label(mainPanel).setText("Apodo");
-		new Label(mainPanel).setText("Handicap");
-		new Label(mainPanel).setText("Promedio del ultimo partido");
-		new Label(mainPanel).setText("Promedio de todos los partidos que jugo");
-		new Label(mainPanel).setText("Fecha de nacimiento");
-		new Label(mainPanel).setText("Amigos");
-		new Label(mainPanel).setText("Infracciones");
-		new Label(mainPanel).setText("Cantidad de partidos que jugo");
+		new Label(mainPanel).setText("Nombre:");
+		new Label(mainPanel).setText(getModelObject().getNombre());
+		new Label(mainPanel).setText("Apodo:");
+		new Label(mainPanel).setText(getModelObject().getApodo());
+		new Label(mainPanel).setText("Handicap:");
+		new Label(mainPanel).setText("Promedio del ultimo partido:");
+		new Label(mainPanel).setText("Promedio de todos los partidos que jugo:");
+		new Label(mainPanel).setText("Fecha de nacimiento:");
+		new Label(mainPanel).setText("Amigos:");
+		new Label(mainPanel).setText("Infracciones:");
+		new Label(mainPanel).setText("Cantidad de partidos que jugo:");
 		
 		new Button(mainPanel)
 			.setCaption("Volver")
--- src/main/java/com/grupo10/ui/JugadorViewModel.java
+++ src/main/java/com/grupo10/ui/JugadorViewModel.java
@@ -2,7 +2,21 @@ package com.grupo10.ui;
 
 import org.uqbar.commons.utils.Observable;
 
+import com.grupo10.juego.Participante;
+
 @Observable
 public class JugadorViewModel {
-
+	Participante jugador;
+	
+	public JugadorViewModel(Participante jugador) {
+		this.jugador = jugador;
+	}
+	
+	public String getNombre() {
+		return jugador.getNombre();
+	}
+	
+	public String getApodo() {
+		return jugador.getApodo();
+	}
 }
